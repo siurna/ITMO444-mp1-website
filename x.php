c8efe0e6c10ea297108510775cf1d6cf7d5e7ac7
Tomas Šiurna <tomassiurna@gmail.com>
2017-11-21 21:01:17 -0600

    Updated PHP

--- x.php
+++ x.php
@@ -1,16 +1,24 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
+     	error_reporting(E_ALL);
+        ini_set('display_errors', 1);
 
-	require 'vendor/autoload.php';
-	use Aws\Ec2\Ec2Client;
+        require 'vendor/autoload.php';
+        use Aws\Ec2\Ec2Client;
+        use Aws\Credentials\CredentialProvider;
+use Aws\Common\Credentials\Credentials;
 
-	$ec2Client = new Ec2Client([
-	    'region' => 'us-west-2',
-	    'version' => '2016-11-15',
-	    'profile' => 'default'
-	]);
+$provider = CredentialProvider::instanceProfile();
+// Be sure to memoize the credentials
+$memoizedProvider = CredentialProvider::memoize($provider);
+
+
+        $ec2Client = new Ec2Client([
+            'region' => 'us-west-2',
+            'version' => '2016-11-15',
+            'profile' => 'default',
+            'credentials' => $memoizedProvider
+        ]);
 	$result = $ec2Client->describeInstances();
-	var_dump($result);
-	echo "SOMEThING";
+        var_dump($result);
+        echo "SOMEThING";
 ?>
