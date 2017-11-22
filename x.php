2ba02458bf91463619ac6a17649d501b1b5eb7c0
Tomas Šiurna <tomassiurna@gmail.com>
2017-11-21 21:53:28 -0600

    Finally made authentification work

--- x.php
+++ x.php
@@ -1,24 +1,16 @@
 <?php
-     	error_reporting(E_ALL);
-        ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
 
-        require 'vendor/autoload.php';
-        use Aws\Ec2\Ec2Client;
-        use Aws\Credentials\CredentialProvider;
-use Aws\Common\Credentials\Credentials;
+	require 'vendor/autoload.php';
+	use Aws\Ec2\Ec2Client;
 
-$provider = CredentialProvider::instanceProfile();
-// Be sure to memoize the credentials
-$memoizedProvider = CredentialProvider::memoize($provider);
-
-
-        $ec2Client = new Ec2Client([
-            'region' => 'us-west-2',
-            'version' => '2016-11-15',
-            'profile' => 'default',
-            'credentials' => $memoizedProvider
-        ]);
+	$ec2Client = new Ec2Client([
+		'region' => 'us-east-1',
+		'version' => '2016-11-15'
+	]);
+	
 	$result = $ec2Client->describeInstances();
-        var_dump($result);
-        echo "SOMEThING";
+	var_dump($result);
+	echo "SOMEThING";
 ?>
