c8640867e1f82ebb62768978124db69f8a4c3db2
Tomas Šiurna <tomassiurna@gmail.com>
2017-11-26 12:12:20 -0600

    Small fixed

--- upload.php
+++ upload.php
@@ -25,27 +25,27 @@
 		}
 	</style>
 
+	<?php require("init.php"); ?>
 </head>
 <body>
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-8 offset-lg-2 text-center">
-				<?if (isset($_POST)):?>
-					<?print_r($_POST);?>
-				<?}?>
+				<?php print_r($_POST);?>
+				<?php print_r($_FILES);?>
 
-				<?if (!getRDShost()):?>
+				<?php if (!getRDShost()):?>
 				<div class="alert alert-dismissible alert-warning" style="margin-bottom: 40px;">
 					<h4>Heads up!</h4>
-					<p>Database for this website is still being loaded, so the upload capability might be restricted. Sorry for any inconveniences and please check again later!</p>
-					<p><a href="." class="btn btn-outline-info">Reload page</a></p>
+					<p>Database for this website is still being loaded, so upload capability might be restricted. Sorry for any inconveniences and please check again later!</p>
+					<p><a href="upload.php" class="btn btn-outline-info">Reload this page</a></p>
 				</div>
 				<?endif;?>
 				
 				<div class="well">
 					<h1>Upload image</h1><hr/>
 
-					<form action="" method="POST">
+					<form action="" method="POST" enctype="multipart/form-data">
 						<div class="row">
 							<div class="col-lg-6">
 								<input type="email" name="email" class="input-lg form-control" placeholder="Your email address">
@@ -59,7 +59,7 @@
 						<div class="row">
 							<div class="text-center col-lg-12" style="margin-top: 25px;">
 								<label for="image" style="margin-right: 10px">Choose file to upload</label>
-								<input type="file" id="image" name="image" multiple>
+								<input type="file" id="image" name="image">
 							</div>
 						</div>
 
